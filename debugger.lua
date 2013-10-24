0c84f2c8dad1288a5f43f18e8e37f8c8f3b4c3cf
Antonio Collins <ramar.collins@gmail.com>
2013-10-24 11:00:36 -0400

    Added a proper debugging routine.
    methods added:
    config(t)
    vert(t)
    horiz(t)
    show(t)
    timing(t)
    
    More to come.

--- debugger.lua
+++ debugger.lua
@@ -3,47 +3,185 @@
 --
 -- Debugging stuff. 
 ------------------------------------------------------
-
+------------------------------------------------------
+-- time {} 
+--
+-- Clocking. 
+------------------------------------------------------
 local time = { 
 	start = 0, 
 	finish = function () return os.time() end
 } 
 
+-- Set up the style block.
+local d_style_block = {}
+
+------------------------------------------------------
+-- cdef {} 
+--
+-- Console defaults. 
+------------------------------------------------------
+local cdef = {	
+name = {
+ console = "__console",
+ timing = "__timing",
+ server_side = "__server_side",
+ tables = "__tables",
+ external = "__external",
+},
+left = "0px",
+padding = "10px",
+bg = "black",
+fg = "white",
+}
+
+------------------------------------------------------
+-- tags {} 
+--
+-- Each tag. 
+------------------------------------------------------
+local tags = {
+-- Strings or Integers
+"name",
+"left", "l",
+"right", "r",
+"width", "w",
+"height", "h",
+"padding", "p",
+"background-color", "bgcolor", "bg",
+"color", "fgcolor", "fg",
+
+-- Table (or string) 
+"omit",
+}
+
+
 return {
-	console = function (s)
-		-- I can bag this style with the framework.
-		P(
-			_.style({type = 'text/css'},[[
-.__console { 
-	position: fixed;
-	bottom: -10px;
-	left: 0px;	
-	z-index: 99;
-	height: 50px;
-	width: 100%;
-	padding: 10px;
-	background-color: black;
-	color: white;
-}
+------------------------------------------------------
+-- .vert(t)
+--
+-- Use a vertical console by default.  Configure any 
+-- specific items via table [t].
+--
+-- *nil 
+------------------------------------------------------
+	vert = function (t)
+		local ctags = table.union(tags, {
+			-- Bools
+			"bottom",
+			"top",
+		})
 
-.__timing {
-	color: green;
-	width: 100px;
-}
-.__server_side {
-}
-.__tables {
-}
-.__external {
-}
-			}]]) ..
+		local this = table.retrieve( ctags, t )
 
-		_.div("__console", table.concat({ 
+		-- Add to the defaults
+		cdef = {
+			height = "100%",
+			width = "200px",
+		}
+
+		-- bottom or top?
+		if this.bottom then 
+		elseif this.top then
+		end
+
+		-- Color stuff.  
+		-- fg = { ... } -- you need something that can check multiple levels of or clauses
+				-- to elaborate a lil more:
+				-- text-color or fgcolor or fg or ...	
+
+		-- Send an inline style. 
+		table.insert( d_style_block, 
+			_.style({ type = "text/css" },
+				table.concat({
+
+					-- Console.
+					(this.name or cdef.name.console) .. " {\n\t",
+						"position: fixed",
+						"z-index: 99",
+						"left: " .. (this.left or cdef.left),						
+						"right: " .. (this.right or cdef.right),						
+						"height: " .. (this.height or cdef.height),						
+						"width: " .. (this.width or cdef.width),						
+						"padding: " .. (this.padding or cdef.padding),						
+					--	"background-color: " .. (this.bg or cdef.bg),						
+						"color: " .. (this.fg or cdef.fg),						
+						";\n}",
+
+					-- Timing if asked for.
+
+					-- Other omits...
+					},";\n\t")))
+	end,
+
+------------------------------------------------------
+-- .horiz(t) 
+--
+-- Use a horizontal console by default.  Configure any 
+-- specific items via table [t].
+--
+-- *nil 
+------------------------------------------------------
+	horiz = function (t)
+		local this = table.retrieve({
+			"name",
+			"left", "l",
+			"right", "r",
+			"width", "w",
+			"height", "h",
+			"padding", "p",
+			"background-color", "bgcolor", "bg",
+			"text-color", "fgcolor", "fg",
+			"omit"
+		},t)
+
+		-- Send an inline style. 
+		table.insert( d_style_block, 
+			_.style({ type = "text/css" },
+				table.concat({
+					"position: fixed",
+					";\n}"
+				},";\n")))
+	end,
+
+------------------------------------------------------
+-- .timing(t)
+--
+-- Fine tune as much data as possible when using the
+-- timing library.
+--
+-- *nil
+------------------------------------------------------
+	timing = function (t)
+	end,
+
+------------------------------------------------------
+-- .config(t)
+--
+-- Don't let kirk configure any console styling for
+-- you.  Do it all yourself.
+--
+-- *nil 
+------------------------------------------------------
+	config = function (t)
+	end,
+
+------------------------------------------------------
+-- .show() 
+--
+-- Show the configured console.
+--
+-- *string 
+------------------------------------------------------
+	show = function (s)
+		return table.concat({
+			table.concat( d_style_block ),
+			_.div("__console", table.concat({ 
 			"???" .. tostring(s),
 			_.div("__timing", "Page load time: " .. 19.12312312312213 .. 
 				(os.time() - TIME) ),
 			_.div("__external", "Application Information: " .. _.i("none")),
-			}))	
-		)
+			}))
+		})
 	end,
 }
